6d288cfc150e6f8e2b3323192f4ffee02ca68eb0
chenjib <chenjibzh@163.com>
2020-06-30 18:23:02 +0800

    服务器启动逻辑改写🎟🎟🎟

--- server.js
+++ server.js
@@ -21,10 +21,11 @@ app.listen(PORT, function (err) {
   console.log('Node app is running on port:', PORT);
 
   // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
+  process.on('uncaughtException', function (err) {
     console.error('Caught exception:', err.stack);
   });
-  process.on('unhandledRejection', function(reason, p) {
+  process.on('unhandledRejection', function (reason, p) {
     console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
   });
+
 });

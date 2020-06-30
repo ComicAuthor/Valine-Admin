deccd78435cf00af2983f54b75e32a6302266534
chenjib <chenjibzh@163.com>
2020-06-30 18:23:40 +0800

    服务器集群配置🎭🎭🎭

--- server-cluster.js
+++ server-cluster.js
@@ -2,7 +2,6 @@ var cluster = require('cluster');
 
 // 进程数量建议设置为可用的 CPU 数量
 var workers = process.env.LEANCLOUD_AVAILABLE_CPUS || 1;
-
 if (cluster.isMaster) {
   for (var i = 0; i < workers; i++) {
     cluster.fork();

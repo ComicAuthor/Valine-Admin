56314d464c78a322b2ef8cde04f251487ac3c142
ComicAuthor <916305619@qq.com>
2022-01-08 15:29:49 +0800

    更新qmsg展示链接形式

--- utilities/send-mail.js
+++ utilities/send-mail.js
@@ -31,8 +31,8 @@ exports.notice = (comment) => {
   let SITE_NAME = process.env.SITE_NAME;
   let NICK = comment.get("nick");
   let COMMENT = comment.get("comment");
-  let POST_URL =
-    process.env.SITE_URL + comment.get("url") + "#" + comment.get("objectId");
+  let POST_URL = process.env.SITE_URL + comment.get("url") + "#" + comment.get("objectId");
+  let POST_URL_QMSG = comment.get("url") + "#" + comment.get("objectId");
   let SITE_URL = process.env.SITE_URL;
 
   let _template =
@@ -43,8 +43,7 @@ exports.notice = (comment) => {
   let emailContent = eval("`" + _template + "`");
 
   let mailOptions = {
-    from:
-      '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">",
+    from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">",
     to: process.env.BLOGGER_EMAIL || process.env.SENDER_EMAIL,
     subject: emailSubject,
     html: emailContent,
@@ -80,8 +79,7 @@ exports.notice = (comment) => {
       function (error, response, body) {
         if (error) return console.log("发送微信提醒异常：", error);
         if (body) body = JSON.parse(body);
-        if (response.statusCode === 200 && body.errmsg === "success")
-          console.log("已发送微信提醒");
+        if (response.statusCode === 200 && body.errmsg === "success") console.log("已发送微信提醒");
         else console.warn("微信提醒失败:", body);
       }
     );
@@ -95,17 +93,13 @@ exports.notice = (comment) => {
         function (error, response, body) {
           if (error) return console.log("调起QQ戳一戳功能异常：", error);
           if (body) body = JSON.parse(body);
-          if (response.statusCode === 200 && body.success === true)
-            console.log("已成功戳一戳！");
+          if (response.statusCode === 200 && body.success === true) console.log("已成功戳一戳！");
           else console.warn("QQ戳一戳失败:", body);
         }
       );
     }
     var comment = $(
-      COMMENT.replace(/<img.*?src="(.*?)".*?>/g, "\n[图片]$1\n").replace(
-        /<br>/g,
-        "\n"
-      )
+      COMMENT.replace(/<img.*?src="(.*?)".*?>/g, "\n[图片]$1\n").replace(/<br>/g, "\n")
     )
       .text()
       .replace(/\n+/g, "\n")
@@ -115,20 +109,17 @@ ${NICK} 给您的回复如下：
            
     ${comment}
         
-您可以点击 ${POST_URL} 前去查看！`;
+您可以点击 ${POST_URL_QMSG} 前去查看！`;
 
     // 自定义模板以及默认模板
     let _template = process.env.QMSG_TEMPLATE || QmsgTemplate;
     request(
-      `https://qmsg.zendee.cn/send/${
-        process.env.QMSG_KEY
-      }?msg=${encodeURIComponent(_template)}`,
+      `https://qmsg.zendee.cn/send/${process.env.QMSG_KEY}?msg=${encodeURIComponent(_template)}`,
       function (error, response, body) {
         if (error) return console.log("发送QQ提醒异常：", error);
         console.log(body);
         if (body) body = JSON.parse(body);
-        if (response.statusCode === 200 && body.success === true)
-          console.log("已发送QQ提醒");
+        if (response.statusCode === 200 && body.success === true) console.log("已发送QQ提醒");
         else console.warn("QQ提醒失败:", body);
       }
     );
@@ -143,25 +134,19 @@ exports.send = (currentComment, parentComment) => {
   let COMMENT = currentComment.get("comment");
   let PARENT_COMMENT = parentComment.get("comment");
   let POST_URL =
-    process.env.SITE_URL +
-    currentComment.get("url") +
-    "#" +
-    currentComment.get("objectId");
+    process.env.SITE_URL + currentComment.get("url") + "#" + currentComment.get("objectId");
   let SITE_URL = process.env.SITE_URL;
   let BLOGGER_EMAIL = process.env.BLOGGER_EMAIL;
   let _subject, _template;
 
   if (MAIL == BLOGGER_EMAIL) {
     _subject =
-      process.env.MAIL_SUBJECT_BLOGGER ||
-      "${PARENT_NICK}，您在『${SITE_NAME}』上的评论收到了回复";
+      process.env.MAIL_SUBJECT_BLOGGER || "${PARENT_NICK}，您在『${SITE_NAME}』上的评论收到了回复";
     _template =
       process.env.MAIL_TEMPLATE_BLOGGER ||
       '<div style="border-radius: 10px 10px 10px 10px;font-size:13px;    color: #555555;width: 666px;font-family:"Century Gothic","Trebuchet MS","Hiragino Sans GB",微软雅黑,"Microsoft Yahei",Tahoma,Helvetica,Arial,"SimSun",sans-serif;margin:50px auto;border:1px solid #eee;max-width:100%;background: #ffffff repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 1px 5px rgba(0, 0, 0, 0.15);"><div style="width:100%;background:#49BDAD;color:#ffffff;border-radius: 10px 10px 0 0;background-image: -moz-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));background-image: -webkit-linear-gradient(0deg, rgb(67, 198, 184), rgb(255, 209, 244));height: 66px;"><p style="font-size:15px;word-break:break-all;padding: 23px 32px;margin:0;background-color: hsla(0,0%,100%,.4);border-radius: 10px 10px 0 0;">您在<a style="text-decoration:none;color: #ffffff;"href="${SITE_URL}">${SITE_NAME}</a>上的留言有新回复啦！</p></div><div style="margin:40px auto;width:90%"><p>${PARENT_NICK}同学，您曾在文章上发表评论：</p><div class="parentComment"style="background: #fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:14px;color:#555555;">${PARENT_COMMENT}</div><p><span class="bloger">博主</span>给您的回复如下：</p><div class="comment"style="background: #fafafa repeating-linear-gradient(-45deg,#fff,#fff 1.125rem,transparent 1.125rem,transparent 2.25rem);box-shadow: 0 2px 5px rgba(0, 0, 0, 0.15);margin:20px 0px;padding:15px;border-radius:5px;font-size:14px;color:#555555;">${COMMENT}</div><p>您可以点击<a style="text-decoration:none; color:#12addb"href="${POST_URL}#comments">查看回复的完整內容</a>，欢迎光临<a style="text-decoration:none; color:#12addb"href="${SITE_URL}">${SITE_NAME}</a>。</p><p>此邮件由系统自动发送，如您从未操作，请忽略并删除！</p><style type="text/css">a:link{text-decoration:none}a:visited{text-decoration:none}a:hover{text-decoration:none}a:active{text-decoration:none}.comment,.parentComment{max-width:100%}.comment img,.parentComment img{width:100%}.vemoji{width:30px;margin:10px 0}.bloger{background-color:#ecf5ff;display:inline-block;height:22px;padding:0 10px;line-height:20px;font-size:12px;color:#409eff;border:1px solid#d9ecff;border-radius:4px;box-sizing:border-box;white-space:nowrap}</style></div></div>';
   } else {
-    _subject =
-      process.env.MAIL_SUBJECT ||
-      "${PARENT_NICK}，您在『${SITE_NAME}』上的评论收到了回复";
+    _subject = process.env.MAIL_SUBJECT || "${PARENT_NICK}，您在『${SITE_NAME}』上的评论收到了回复";
     _template =
       process.env.MAIL_TEMPLATE ||
       '<div style="border-top:2px solid #12ADDB;box-shadow:0 1px 3px #AAAAAA;line-height:180%;padding:0 15px 12px;margin:50px auto;font-size:12px;"><h2 style="border-bottom:1px solid #DDD;font-size:14px;font-weight:normal;padding:13px 0 10px 8px;">        您在<a style="text-decoration:none;color: #12ADDB;" href="${SITE_URL}" target="_blank">            ${SITE_NAME}</a>上的评论有了新的回复</h2>    ${PARENT_NICK} 同学，您曾发表评论：<div style="padding:0 12px 0 12px;margin-top:18px"><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">            ${PARENT_COMMENT}</div><p><strong>${NICK}</strong>回复说：</p><div style="background-color: #f5f5f5;padding: 10px 15px;margin:18px 0;word-wrap:break-word;">            ${COMMENT}</div><p>您可以点击<a style="text-decoration:none; color:#12addb" href="${POST_URL}" target="_blank">查看回复的完整內容</a>，欢迎再次光临<a style="text-decoration:none; color:#12addb" href="${SITE_URL}" target="_blank">${SITE_NAME}</a>。<br></p></div></div>';
@@ -170,8 +155,7 @@ exports.send = (currentComment, parentComment) => {
   let emailContent = eval("`" + _template + "`");
 
   let mailOptions = {
-    from:
-      '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">", // sender address
+    from: '"' + process.env.SENDER_NAME + '" <' + process.env.SENDER_EMAIL + ">", // sender address
     to: parentComment.get("mail"),
     subject: emailSubject,
     html: emailContent,
